6cd7515ece0be940552bfb94db2f3ef8cd4e685a
Shy <shyxlloyd@gmail.com>
2023-07-07 07:04:03 +0100

    Export this so index can call it

--- deploy.js
+++ deploy.js
@@ -3,35 +3,37 @@ const { client, guild, token } = require('./config.json');
 const fs = require('node:fs');
 const path = require('node:path');
 
-const commands = [];
+module.exports = async function deployCommands() {
+	const commands = [];
 
-const commandsPath = path.join(__dirname, 'commands');
-const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+	const commandsPath = path.join(__dirname, 'commands');
+	const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
 
-for (const file of commandFiles) {
-	const filePath = path.join(commandsPath, file);
-	const command = require(filePath);
+	for (const file of commandFiles) {
+		const filePath = path.join(commandsPath, file);
+		const command = require(filePath);
 
-	if ('data' in command && 'execute' in command) {
-		commands.push(command.data.toJSON());
+		if ('data' in command && 'execute' in command) {
+			commands.push(command.data.toJSON());
+		}
+		else {
+			console.log(`[WARNING] The command at ${filePath} is missing a required property. Required properites: data, execute.`);
+		}
 	}
-	else {
-		console.log(`[WARNING] The command at ${filePath} is missing a required property. Required properites: data, category, execute.`);
-	}
-}
 
-const rest = new REST().setToken(token);
+	const rest = new REST().setToken(token);
 
-(async () => {
-	try {
-		console.log(`Started refreshing ${commands.length} application commands.`);
+	(async () => {
+		try {
+			console.log(`Started refreshing ${commands.length} application commands.`);
 
-		await rest.put(
-			Routes.applicationGuildCommands(client, guild),
-			{ body: commands },
-		);
-	}
-	catch (error) {
-		console.error(error);
-	}
-})();
+			await rest.put(
+				Routes.applicationCommands(client),
+				{ body: commands },
+			);
+		}
+		catch (error) {
+			console.error(error);
+		}
+	})();
+};
