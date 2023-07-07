5e3d3f54cf0c5c2a8eb771acc8dfca5be2e7c482
Shy <shyxlloyd@gmail.com>
2023-07-07 07:03:31 +0100

    Save extra information on stickers

--- index.js
+++ index.js
@@ -35,6 +35,8 @@ client.on(Events.MessageCreate, async message => {
 	const serverTable = db.table(`stickers_${message.guildId}`);
 
 	await serverTable.add(`${sticker.id}.uses`, 1)
+	await serverTable.set(`${sticker.id}.name`, sticker.name);
+	await serverTable.set(`${sticker.id}.url`, sticker.url);
 
 	if (!sticker.guildId) {
 		await serverTable.set(`${sticker.id}.guild`, 'default');
