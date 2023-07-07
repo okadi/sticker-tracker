a08b6f342fdf362363a2778792d370bb2dd082f6
Shy <shyxlloyd@gmail.com>
2023-07-07 08:50:50 +0100

    Add sort order option

--- commands/stickers.js
+++ commands/stickers.js
@@ -9,36 +9,59 @@ module.exports = {
 		.setDescription('Provides stats on sticker usage.')
 		.setDMPermission(false)
 		.addSubcommand(subcommand => subcommand
-			.setName('top')
-			.setDescription('Sort from top to bottom.'))
+			.setName('list')
+			.setDescription('List the sticker usage for this server.')
+			.addStringOption(option => option
+				.setName('sort')
+				.setDescription('Sort order')
+				.setRequired(true)
+				.addChoices(
+					{ name: 'Most Used', value: 'top' },
+					{ name: 'Least Used', value: 'bottom' },
+				))
+			.addBooleanOption(option => option
+				.setName('external')
+				.setDescription('Include stickers from other servers in the listing.')))
 		.addSubcommand(subcommand => subcommand
-			.setName('bottom')
-			.setDescription('Sort from bottom to top.')),
+			.setName('stats')
+			.setDescription('Show some statistics for this server.')),
 	async execute(interaction) {
+		await interaction.deferReply();
+
 		const serverTable = db.table(`stickers_${interaction.guildId}`);
         const allData = await serverTable.all();
-
 		const filteredData = allData.filter(entry => entry.value.guild === interaction.guild.id);
-		const sortedData = filteredData.sort((a, b) => b.value.uses - a.value.uses);
 
-		const topEntries = sortedData.slice(0, 5);
+		let sortedData = filteredData.sort((a, b) => b.value.uses - a.value.uses);
+		if (interaction.options.getBoolean('external')) {
+			sortedData = allData.sort((a, b) => b.value.uses - a.value.uses);
+		};
+
+		const entries = (interaction.options.getString('sort') === 'top') ? sortedData.slice(0, 10) : sortedData.slice(-10);
+
+		if (entries.length === 0) {
+			return await interaction.editReply({ content: `No stickers have been logged yet!` });
+		};
 
 		let list = '';
-		for (const entry of topEntries) {
+		for (const entry of entries) {
 			try {
 				sticker = await interaction.client.fetchSticker(entry.id);
 			} catch(error) {
-				console.error(`Sticker ${entry.id} no longer exists!`);
+				// Sticker likely no longer exists
+				if (entry.value.name) {
+					list += `~~${entry.value.name} - **${entry.value.uses}** uses~~\n`
+				}
 				continue;
 			}
 
 			list += `[${sticker.name}](${sticker.url}) - **${entry.value.uses}** uses\n`;
 		};
 
-        const embed = new EmbedBuilder()
-			.setTitle('Top 5 Stickers')
+        const listEmbed = new EmbedBuilder()
+			.setTitle(`Sticker listing for ${interaction.guild.name}`)
 			.setDescription(list)
 
-		await interaction.reply({ embeds: [embed] });
+		await interaction.editReply({ embeds: [listEmbed] });
 	},
 };
